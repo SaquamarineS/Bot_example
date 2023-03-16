614266f9aaa28ef3e1849c6c6ba5479cd25a4209
Axis Wishful <71726228+SaquamarineS@users.noreply.github.com>
2023-03-16 19:39:13 +0600

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,39 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+
+TOKEN = '6090188735:AAE-7Q6frjzI4-WiH8cIgBG1fEJSbNYsW30'
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+
+
+@dp.message_handler(commands=['start'])
+async def start_func(message : types.Message):
+    await message.raply('Starter')
+    
+    
+    
+@dp.message_handler(commands=['help'])
+async def start_func(message : types.Message):
+    await message.raply('Helper')
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+
+
+
+
+
+
+
+
+
+
+
+
